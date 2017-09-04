f51753b37074b669211adba9322a62b6ee217663
Steve Zaske <github@zaske.com>
2017-09-04 10:23:45 -0700

    Initial commit and file structure

---
+++ app/src/main/java/com/zaske/szaske/basic_kneads_pizza/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zaske.szaske.basic_kneads_pizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
