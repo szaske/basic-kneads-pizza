e140b4373e2f50968789947bfab933bcdf07a528
Steve Zaske <github@zaske.com>
2017-09-04 21:58:01 -0700

    Add zip code entry and logging to logcat

--- app/src/main/java/com/zaske/szaske/basic_kneads_pizza/MainActivity.java
+++ app/src/main/java/com/zaske/szaske/basic_kneads_pizza/MainActivity.java
@@ -12,24 +12,31 @@ import com.facebook.common.util.UriUtil;
 //import java.net.URI;
 
 import android.net.Uri;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //Declare our variables
+    private static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String locationZipCode = mLocationEditText.getText().toString();
+                Log.d(TAG,"Zip code entered was: " + locationZipCode);
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 startActivity(intent);
             }
